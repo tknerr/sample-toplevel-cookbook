3a7cb7ec0c5c83dd1bd210b4d960b23ef61259ba
Torben Knerr <mail@tknerr.de>
2014-07-17 02:05:22 +0200

    bump cookbook version to dev (odd) version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "tkn@zuehlke.com"
 license          "All rights reserved"
 description      "A minimal sample application cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 # 
 # dependencies on "library cookbooks"
