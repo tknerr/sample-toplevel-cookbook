755621243b7dbce2d97f7ec0eff40248fde02c06
Torben Knerr <mail@tknerr.de>
2014-10-31 23:16:06 +0100

    update to serverspec v2 compatible spec_helper.rb, see http://bit.ly/10bmB0r

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,11 +1,3 @@
 require 'serverspec'
-require 'pathname'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-end
+set :backend, :exec
