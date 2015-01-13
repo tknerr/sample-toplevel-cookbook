9d5d2384640583e8507ed3081d8221584ceec4b8
Torben Knerr <mail@tknerr.de>
2015-01-13 07:51:55 +0100

    quick workaround to ensure that netstat is installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,9 @@
 # refresh apt cache
 include_recipe "apt"
 
+# XXX: ensure netstat is installed because it's required by the serverspec tests
+package "net-tools"
+
 # ensure default site is enabled and install apache
 node.set['apache']['default_site_enabled'] = true
 include_recipe "apache2"
