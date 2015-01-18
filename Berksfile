3abaf1a948f4b724499fbf217d987c74747f703e
Torben Knerr <mail@tknerr.de>
2015-01-18 00:02:07 +0000

    back to traditional approach -- otherwise updating the cookbook dependencies would not work without removing Berksfile.lock first

--- Berksfile
+++ Berksfile
@@ -1,12 +1,3 @@
 source "https://supermarket.chef.io"
 
-#
-# dependencies on "application cookbooks"
-#
-cookbook "apache2",  "1.10.4"
-cookbook "apt",      "2.4.0"
-
-#
-# dependency on self
-#
-cookbook "sample-app", path: "."
+metadata
--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,5 @@ description      "A minimal sample application cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
 
-
-# read resolved dependencies from Berksfile.lock, i.e. lock the complete dependency graph
-locked_deps = IO.read('Berksfile.lock').lines.drop_while { |l| l != "GRAPH\n" }
-locked_deps.keep_if { |line| line.match(/^\s{2}\w+\s\(.*\)/) }.each do |entry|
-  dep_name, dep_version = entry.strip.match(/^(\w+)\s\((.*)\)/).captures
-  depends(dep_name, dep_version) unless dep_name.eql? name
-end
+depends "apache2", "1.10.4"
+depends "apt", "2.4.0"
