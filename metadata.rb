88a983ff3a207b1ee1a0f01ef7cf66bf10f5e786
Torben Knerr <mail@tknerr.de>
2015-01-18 00:03:15 +0000

    update to latest version of apache2 and apt cookbooks

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      "A minimal sample application cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
 
-depends "apache2", "1.10.4"
-depends "apt", "2.4.0"
+depends "apache2", "3.0.0"
+depends "apt", "2.6.1"
